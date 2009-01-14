d7318d3dabec112953e8f0f357bd39fb473cc857
Kevin Old <kevin@kevinold.com>
2009-01-14 14:21:08 -0600

    WIP - stripping out address, city, state, zip info

--- parse_table.pl
+++ parse_table.pl
@@ -8,8 +8,16 @@ use HousesDB;
 my $schema = HousesDB->connect('dbi:SQLite:houses.db');
 my $agent = WWW::Mechanize->new( autocheck => 1 );
 $agent->env_proxy();
-
 $agent->get('http://www.mlsfinder.com/tn_mtrmls/kw_493/index.cfm?action=listing_detail&property_id=1032268');
+
+my $c = $agent->content;
+$c =~ m{<span class="detail_detail" style="font-size: 12px; font-weight: bold;">(.*?)</span>}s;
+my $detail_detail = $1;
+$detail_detail =~ s/\t+|\s{2,}//g;
+$detail_detail =~ s/<div style="float: right;"><\/div>//;
+my ($address, $city_state_zip) = split /\<br\>/, $detail_detail;
+warn $city_state_zip;
+
 my @items = ( 'Year Built','Square Feet','Total Full Baths','Total Half Baths', 'Bedrooms', 'Subdivision', 'Acres', 'Listing Price' );
 my $table = HTML::TableExtract->new();
 (my $content = $agent->content) =~ s/\&nbsp;?//g;
