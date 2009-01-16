4c545af991a54dabbafc77adf0f3e4e56e8e2f5f
Kevin Old <kevin@kevinold.com>
2009-01-16 17:31:45 -0600

    WIP - view created, model moved into lib

---
+++ lib/houses/Model/DB.pm
@@ -0,0 +1,26 @@
+package houses::Model::DB;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Model';
+
+=head1 NAME
+
+houses::Model::DB - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Kevin Old
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ lib/houses/View/TT.pm
@@ -0,0 +1,31 @@
+package houses::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+houses::View::TT - TT View for houses
+
+=head1 DESCRIPTION
+
+TT View for houses. 
+
+=head1 AUTHOR
+
+=head1 SEE ALSO
+
+L<houses>
+
+Kevin Old
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
